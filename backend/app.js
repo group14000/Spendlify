a5e16ef262f328ceeea984e8bc84532c40fe830e
DIGONTA HALDER <digantahalder14000@gmail.com>
2024-07-25 22:33:24 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-const expenseRoutes = require("./routes/expenseRoutes");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const cors = require("cors");
@@ -19,7 +18,6 @@ app.use(cors()); // Enable CORS for all routes
 
 // Define Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/expenses", expenseRoutes);
 
 const PORT = process.env.PORT;
 
